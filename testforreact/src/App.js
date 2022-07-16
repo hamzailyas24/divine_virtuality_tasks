9d31abb11f99081f4b8d01ce6eba9dfcda7568ea
Hamza Ilyas <hamza24061@gmail.com>
2022-07-17 01:41:19 +0500

    Task One Completed

---
+++ testforreact/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Taskone from "./task01/Taskone";
+
+function App() {
+  return (
+    <>
+      <Taskone />
+    </>
+  );
+}
+
+export default App;
---
+++ testforreact/src/task01/Taskone.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Button, Container } from "react-bootstrap";
+
+function Taskone() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <>
+      <Container>
+
+        <h1 className="text-center mt-4">TASK 01</h1>
+
+        <h1 className="text-danger text-center">{count}</h1>
+        <div className="d-flex justify-content-center align-items-center">
+          {count < 10 ? (
+            <Button variant="primary" className="m-2" onClick={() => setCount(count + 1)}>
+              INCREMENT
+            </Button>
+          ) : null}
+
+          {count > 0 ? (
+            <Button variant="danger" onClick={() => setCount(count - 1)}>
+              Decrement
+            </Button>
+          ) : null}
+        </div>
+      </Container>
+    </>
+  );
+}
+
+export default Taskone;
